987b75a87c242bb720c28e559885b8129b77af53
Damislav <idamjanovic28@gmail.com>
2021-03-18 15:51:36 +0100

    ##

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-// listeners that listens on submit
+//by default submit button should be disabled
+document.querySelector("#submit").disabled = true;
+
+document.querySelector("#task").onkeyup = () => {
+  if (document.querySelector("#task").value.length > 0) {
+    document.querySelector("#submit").disabled = false;
+  } else {
+    document.querySelector("#submit").disabled = true;
+  }
+};
+// listeners that listens on form submit
 
 document.querySelector("form").onsubmit = () => {
   const task = document.querySelector("#task").value;
-
+  // create li element
   const li = document.createElement("li");
+
+  // append value of input to the new li
   li.innerHTML = task;
 
-  document.querySelector("ul").appendChild(li);
-  task.value = "";
+  // select ul tasks and append li to it
+
+  document.querySelectorAll("#tasks").forEach((item) => {
+    let deleteBtn = document.createElement("span");
+    deleteBtn.innerHTML = "X";
+    deleteBtn.classList = "deleteBtn";
+    li.appendChild(deleteBtn);
+    item.appendChild(li);
+
+    deleteBtn.addEventListener("click", (e) => {
+      li.classList = "remove";
+    });
+  });
+
+  //select input value and reset it
+  document.querySelector("#task").value = "";
+
+  document.querySelector("#submit").disabled = true;
 
   // stop form from submitting
   return false;
