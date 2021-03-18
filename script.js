2580c6e0d9ed644fa79b15329c0f8bd350c4cb94
Damislav <idamjanovic28@gmail.com>
2021-03-18 19:45:05 +0100

    #

--- script.js
+++ script.js
@@ -61,10 +61,34 @@ function count() {
   document.querySelector(".counter").innerHTML = counter;
   localStorage.setItem("counter", counter);
 }
- 
 
-// accsess counter element and give it localstorage 
+// accsess counter element and give it localstorage
 document.querySelector(".counter").innerHTML = localStorage.getItem("counter");
 document.querySelector("button").addEventListener("click", count);
 
 // call  count every second
+
+document.querySelector("#currency-form").onsubmit = () => {
+  fetch("https://api.exchangeratesapi.io/latest?base=USD")
+    .then((response) => response.json())
+    .then((data) => {
+      // user inputs
+      let currency = document.querySelector("#currency").value.toUpperCase();
+
+      const rate = data.rates[currency]; //access at the position  input of currency
+      if (rate !== undefined) {
+        document.querySelector("#result").innerHTML = `1 USD is ${rate.toFixed(
+          3
+        )}
+            ${currency}
+        `;
+      } else {
+        document.querySelector("#result").innerHTML = "Invalid currency";
+      }
+    })
+    .catch((err) => {
+      console.log("Error", err);
+    });
+
+  return false;
+};
